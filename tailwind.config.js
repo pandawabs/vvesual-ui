2bbc2bfd4a7f7f179adc74e464966b39cdb3b529
Pandawa Sudewa <pandawa.bs@gmail.com>
2023-12-03 11:09:41 +0700

    Extends base colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.sky,
+        secondary: colors.slate,
+        success: colors.green,
+        danger: colors.red,
+        warning: colors.yellow,
+        info: colors.teal,
+      }
+    },
   },
   plugins: [],
 }
