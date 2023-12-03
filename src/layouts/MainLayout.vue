c9022a0a4faf233eb0db739a10b0ac2d372c8b2e
Pandawa Sudewa <pandawa.bs@gmail.com>
2023-12-03 11:13:49 +0700

    Update structure and style

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,11 +1,11 @@
 <script setup></script>
 
 <template>
-  <div
+  <main
     class="border-slate-950 bg-slate-50 p-4 pt-36 md:rounded-2xl md:border-8 md:p-16"
   >
     <router-view />
-  </div>
+  </main>
 </template>
 
 <style scoped></style>
