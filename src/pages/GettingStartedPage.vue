72aaa77dc651155ab7b4ee8f64688db07ac1a6de
Pandawa Sudewa <pandawa.sudewa@hariffdp.com>
2023-12-08 18:09:44 +0700

    word wrap

--- src/pages/GettingStartedPage.vue
+++ src/pages/GettingStartedPage.vue
@@ -1,18 +1,48 @@
 <script setup>
-import LinkElement from "../elements/LinkElement.vue"
+import LinkElement from "../elements/LinkElement.vue";
 </script>
 
 <template>
   <h1>🚀 Getting Started</h1>
   <h2>Introduction</h2>
-  <p>Welcome to VVESUAL UI, a powerful and versatile UI Kit built on top of <LinkElement href="https://tailwindcss.com/" title="Tailwind CSS Website" rel="noreferrer nofollow">Tailwind CSS</LinkElement>. VVESUAL UI provides a collection of meticulously designed components to accelerate your web development projects while maintaining flexibility and ease of customization.</p>
+  <p>
+    VVESUAL UI is a powerful and versatile UI Kit built on top of
+    <LinkElement
+      href="https://tailwindcss.com/"
+      title="Tailwind CSS Website"
+      rel="noreferrer nofollow"
+      >Tailwind CSS</LinkElement
+    >. VVESUAL UI provides a collection of meticulously designed components to
+    accelerate your web development projects while maintaining flexibility and
+    ease of customization.
+  </p>
   <h2>Key Features</h2>
   <h3>Comprehensive Components</h3>
-  <p>VVESUAL UI boasts an extensive library of meticulously crafted components, covering buttons, forms, navigation elements, alerts, cards, typography, and icons. This comprehensive set ensures that developers have everything they need to build visually appealing and functional interfaces without the need for extensive customization.</p>
+  <p>
+    VVESUAL UI boasts an extensive library of meticulously crafted components,
+    covering buttons, forms, navigation elements, alerts, cards, typography, and
+    icons. This comprehensive set ensures that developers have everything they
+    need to build visually appealing and functional interfaces without the need
+    for extensive customization.
+  </p>
   <h3>Seamless Integration</h3>
-  <p>VVESUAL UI seamlessly integrates with Tailwind CSS, harnessing its utility-first approach for simplified styling. Developers can leverage the power of Tailwind CSS alongside VVESUAL UI to achieve consistent, responsive, and visually pleasing designs effortlessly. This integration streamlines the development process, allowing for rapid implementation of UI elements.</p>
+  <p>
+    VVESUAL UI seamlessly integrates with Tailwind CSS, harnessing its
+    utility-first approach for simplified styling. Developers can leverage the
+    power of Tailwind CSS alongside VVESUAL UI to achieve consistent,
+    responsive, and visually pleasing designs effortlessly. This integration
+    streamlines the development process, allowing for rapid implementation of UI
+    elements.
+  </p>
   <h3>High Customization</h3>
-  <p>One of VVESUAL UI's standout features is its high level of customization. Tailor the UI Kit to fit your project's unique design requirements with ease. From color palettes and typography to theming and component extensions, VVESUAL UI provides a flexible framework that empowers developers to express their creativity and maintain a cohesive design language across their applications.</p>
+  <p>
+    One of VVESUAL UI's standout features is its high level of customization.
+    Tailor the UI Kit to fit your project's unique design requirements with
+    ease. From color palettes and typography to theming and component
+    extensions, VVESUAL UI provides a flexible framework that empowers
+    developers to express their creativity and maintain a cohesive design
+    language across their applications.
+  </p>
 </template>
 
 <style scoped></style>
