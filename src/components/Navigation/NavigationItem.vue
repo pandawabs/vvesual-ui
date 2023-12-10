859bc74f5b198a22b8ea329177e7e413fcc31754
Pandawa Sudewa <pandawa.bs@gmail.com>
2023-12-10 11:20:58 +0700

    Update style

--- src/components/Navigation/NavigationItem.vue
+++ src/components/Navigation/NavigationItem.vue
@@ -7,7 +7,7 @@ import NavigationCore from "./NavigationCore.vue";
     v-bind="$attrs"
     class="rounded-full px-5 py-4 font-mono"
     active-class="font-bold bg-primary-400"
-    inactive-class="hover:bg-white/10 hover:font-semibold hover:text-primary-400"
+    inactive-class="hover:font-semibold hover:text-primary-400"
   >
     <slot />
   </NavigationCore>
--- src/layouts/CardLayout.vue
+++ src/layouts/CardLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="z-10 my-4 rounded-2xl border-4 border-dashed border-slate-900 bg-white p-8 drop-shadow-lg"
+    class="z-10 my-4 rounded-2xl border-4 border-dashed border-slate-900 bg-white drop-shadow-lg"
   >
     <slot />
   </div>
